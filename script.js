05ed8f0aa405251ae473e9a7dca67c0a1de0e3be
bluetrumpet <jameschristaldi08@gmail.com>
2025-09-15 09:12:58 -0400

    Update script.js
    9/15 site upload

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 const winds = document.getElementById('winds');
-const activeDiv = winds.closest('.active');
+const pressure = document.getElementById('pressure');
+const dir = document.getElementById('speed');
 
+// winds
 winds.addEventListener('mouseenter', () => {
-  activeDiv.style.width = '50%';
+  document.getElementById("defineWinds").style.display = "block";
+  document.getElementById("active").style.width = "40%";
 });
+
 winds.addEventListener('mouseleave', () => {
-  activeDiv.style.width = '30%';
+  document.getElementById("defineWinds").style.display = "none";
+  document.getElementById("active").style.width = "30%";
+});
+
+pressure.addEventListener('mouseenter', () => {
+  document.getElementById("definePressure").style.display = "block";
+  document.getElementById("active").style.width = "40%";
+});
+
+pressure.addEventListener('mouseleave', () => {
+  document.getElementById("definePressure").style.display = "none";
+  document.getElementById("active").style.width = "30%";
+});
+
+dir.addEventListener('mouseenter', () => {
+  document.getElementById("defineDir").style.display = "block";
+  document.getElementById("active").style.width = "40%";
+});
+
+dir.addEventListener('mouseleave', () => {
+  document.getElementById("defineDir").style.display = "none";
+  document.getElementById("active").style.width = "30%";
 });
